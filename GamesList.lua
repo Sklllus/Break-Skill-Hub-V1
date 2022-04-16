d3769c8e29fa95f27213665a779373ccf8805882
Skillus <patrykstec042003@wp.pl>
2022-04-16 15:37:19 +0200

    Delete GamesList.lua

--- GamesList.lua
+++
@@ -1,8 +0,0 @@
-local Games = {
-    [301549746] = {Name = "Counter Blox", Script = "https://raw.githubusercontent.com/Sklllus/Break-Skill-Hub-V1/main/Break-Skill%20Hub%20-%20V1.lua", Working = true},
-    [5325113759] = {Name = "Counter Blox Trading", Script = "https://raw.githubusercontent.com/Sklllus/Break-Skill-Hub-V1/main/Break-Skill%20Hub%20-%20V1.lua", Working = true},
-    [1869597719] = {Name = "Counter Blox Deathmatch", Script = "https://raw.githubusercontent.com/Sklllus/Break-Skill-Hub-V1/main/Break-Skill%20Hub%20-%20V1.lua", Working = true},
-    [1480424328] = {Name = "Counter Blox UnRanked Competitive", Script = "https://raw.githubusercontent.com/Sklllus/Break-Skill-Hub-V1/main/Break-Skill%20Hub%20-%20V1.lua", Working = true}
-}
-
-return Games
