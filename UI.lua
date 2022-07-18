8cace5fc9ecf61d351202e29ee896ac63173272e
Skillus <patrykstec042003@wp.pl>
2022-07-18 13:35:36 +0200

    Update UI.lua

--- UI.lua
+++ UI.lua
@@ -147,109 +147,107 @@ function library:CreateWindow(options)
 
     local BreakSkillHub = Instance.new("ScreenGui", GUI_Parent)
 
-    local Instances = {
-        ["ImageLabel_1"] = Instance.new("ImageLabel"),
-        ["Frame_3"] = Instance.new("Frame"),
-        ["UICorner_3"] = Instance.new("UICorner"),
-        ["TextLabel_1"] = Instance.new("TextLabel"),
-        ["UICorner_1"] = Instance.new("UICorner"),
-        ["UICorner_2"] = Instance.new("UICorner"),
-        ["ImageLabel_2"] = Instance.new("ImageLabel"),
-        ["UIGradient_2"] = Instance.new("UIGradient"),
-        ["Frame_2"] = Instance.new("Frame"),
-        ["TextLabel_2"] = Instance.new("TextLabel"),
-        ["UIGradient_1"] = Instance.new("UIGradient"),
-        ["Frame_1"] = Instance.new("Frame")
-    }
+    local ImageLabel_1 = Instance.new("ImageLabel")
+    local Frame_3 = Instance.new("Frame")
+    local UICorner_3 = Instance.new("UICorner")
+    local TextLabel_1 = Instance.new("TextLabel")
+    local UICorner_1 = Instance.new("UICorner")
+    local UICorner_2 = Instance.new("UICorner")
+    local ImageLabel_2 = Instance.new("ImageLabel")
+    local UIGradient_2 = Instance.new("UIGradient")
+    local Frame_2 = Instance.new("Frame")
+    local TextLabel_2 = Instance.new("TextLabel")
+    local UIGradient_1 = Instance.new("UIGradient")
+    local Frame_1 = Instance.new("Frame")
 
     BreakSkillHub.Name = "Break-Skill Hub - V1"
     BreakSkillHub.ZIndexBehavior = Enum.ZIndexBehavior.Sibling
     BreakSkillHub.DisplayOrder = 10
     BreakSkillHub.ResetOnSpawn = false
 
-    Instances.Frame_1.Name = "MainFrame"
-    Instances.Frame_1.Parent = BreakSkillHub
-    Instances.Frame_1.BackgroundColor3 = Colors.Main
-    Instances.Frame_1.BorderSizePixel = 0
-    Instances.Frame_1.Position = UDim2.new(0.31683921813964844, 0, 0.09101516753435135, 0)
-    Instances.Frame_1.Size = UDim2.new(0, 490, 0, 700)
+    Frame_1.Name = "MainFrame"
+    Frame_1.Parent = BreakSkillHub
+    Frame_1.BackgroundColor3 = Colors.Main
+    Frame_1.BorderSizePixel = 0
+    Frame_1.Position = UDim2.new(0.31683921813964844, 0, 0.09101516753435135, 0)
+    Frame_1.Size = UDim2.new(0, 490, 0, 700)
 
-    MakeDraggable(Instances.Frame_1, Instances.Frame_1)
+    MakeDraggable(Frame_1, Frame_1)
 
-    Instances.UICorner_1.Name = "UICorner_1"
-    Instances.UICorner_1.Parent = Instances.Frame_1
+    UICorner_1.Name = "UICorner_1"
+    UICorner_1.Parent = Frame_1
 
-    Instances.Frame_2.Name = "Splitter_1"
-    Instances.Frame_2.Parent = Instances.Frame_1
-    Instances.Frame_2.BorderSizePixel = 0
-    Instances.Frame_2.Position = UDim2.new(0, 0, 0.04374168813228607, 0)
-    Instances.Frame_2.Size = UDim2.new(0, 490, 0, 2)
+    Frame_2.Name = "Splitter_1"
+    Frame_2.Parent = Frame_1
+    Frame_2.BorderSizePixel = 0
+    Frame_2.Position = UDim2.new(0, 0, 0.04374168813228607, 0)
+    Frame_2.Size = UDim2.new(0, 490, 0, 2)
 
-    Instances.UIGradient_1.Name = "UIGradient_1"
-    Instances.UIGradient_1.Parent = Instances.Frame_2
-    Instances.UIGradient_1.Color = ColorSequence.new{
+    UIGradient_1.Name = "UIGradient_1"
+    UIGradient_1.Parent = Frame_2
+    UIGradient_1.Color = ColorSequence.new{
         ColorSequenceKeypoint.new(0, Color3.fromRGB(125, 0, 0)),
         ColorSequenceKeypoint.new(0.410982, Color3.fromRGB(78, 39, 39)),
         ColorSequenceKeypoint.new(0.597338, Color3.fromRGB(57, 57, 57)),
         ColorSequenceKeypoint.new(1, Color3.fromRGB(79, 79, 79))
     }
 
-    Instances.TextLabel_1.Name = "WindowName"
-    Instances.TextLabel_1.Parent = Instances.Frame_1
-    Instances.TextLabel_1.BackgroundTransparency = 1
-    Instances.TextLabel_1.BorderSizePixel = 0
-    Instances.TextLabel_1.Text = WindowName
-    Instances.TextLabel_1.TextColor3 = Colors.Text
-    Instances.TextLabel_1.TextSize = 14
-    Instances.TextLabel_1.Font = Enum.Font.GothamSemibold
-    Instances.TextLabel_1.TextXAlignment = Enum.TextXAlignment.Left
-    Instances.TextLabel_1.Position = UDim2.new(0.09183673560619354, 0, 0, 0)
-    Instances.TextLabel_1.Size = UDim2.new(0, 141, 0, 30)
+    TextLabel_1.Name = "WindowName"
+    TextLabel_1.Parent = Frame_1
+    TextLabel_1.BackgroundTransparency = 1
+    TextLabel_1.BorderSizePixel = 0
+    TextLabel_1.Text = WindowName
+    TextLabel_1.TextColor3 = Colors.Text
+    TextLabel_1.TextSize = 14
+    TextLabel_1.Font = Enum.Font.GothamSemibold
+    TextLabel_1.TextXAlignment = Enum.TextXAlignment.Left
+    TextLabel_1.Position = UDim2.new(0.09183673560619354, 0, 0, 0)
+    TextLabel_1.Size = UDim2.new(0, 141, 0, 30)
 
-    Instances.ImageLabel_1.Name = "WindowLogo"
-    Instances.ImageLabel_1.Parent = Instances.Frame_1
-    Instances.ImageLabel_1.BackgroundTransparency = 1
-    Instances.ImageLabel_1.BorderSizePixel = 0
-    Instances.ImageLabel_1.Image = WindowLogo
-    Instances.ImageLabel_1.Size = UDim2.new(0, 35, 0, 30)
+    ImageLabel_1.Name = "WindowLogo"
+    ImageLabel_1.Parent = Frame_1
+    ImageLabel_1.BackgroundTransparency = 1
+    ImageLabel_1.BorderSizePixel = 0
+    ImageLabel_1.Image = WindowLogo
+    ImageLabel_1.Size = UDim2.new(0, 35, 0, 30)
 
-    Instances.UICorner_2.Name = "UICorner_2"
-    Instances.UICorner_2.Parent = Instances.ImageLabel_1
-    Instances.UICorner_2.CornerRadius = UDim.new(1, 4)
+    UICorner_2.Name = "UICorner_2"
+    UICorner_2.Parent = ImageLabel_1
+    UICorner_2.CornerRadius = UDim.new(1, 4)
 
-    Instances.ImageLabel_2.Name = "UserAvatar"
-    Instances.ImageLabel_2.Parent = Instances.Frame_1
-    Instances.ImageLabel_2.BackgroundTransparency = 1
-    Instances.ImageLabel_2.BorderSizePixel = 0
-    Instances.ImageLabel_2.Position = UDim2.new(0.9285714030265808, 0, 0, 0)
-    Instances.ImageLabel_2.Size = UDim2.new(0, 35, 0, 30)
-    Instances.ImageLabel_2.Image = Players:GetUserThumbnailAsync(ClientUserId, ThumbType, ThumbSize)
+    ImageLabel_2.Name = "UserAvatar"
+    ImageLabel_2.Parent = Frame_1
+    ImageLabel_2.BackgroundTransparency = 1
+    ImageLabel_2.BorderSizePixel = 0
+    ImageLabel_2.Position = UDim2.new(0.9285714030265808, 0, 0, 0)
+    ImageLabel_2.Size = UDim2.new(0, 35, 0, 30)
+    ImageLabel_2.Image = Players:GetUserThumbnailAsync(ClientUserId, ThumbType, ThumbSize)
 
-    Instances.UICorner_3.Name = "UICorner_3"
-    Instances.UICorner_3.Parent = Instances.ImageLabel_2
-    Instances.UICorner_3.CornerRadius = UDim.new(1, 4)
+    UICorner_3.Name = "UICorner_3"
+    UICorner_3.Parent = ImageLabel_2
+    UICorner_3.CornerRadius = UDim.new(1, 4)
 
-    Instances.TextLabel_2.Name = "UserName"
-    Instances.TextLabel_2.Parent = Instances.Frame_1
-    Instances.TextLabel_2.BackgroundTransparency = 1
-    Instances.TextLabel_2.BorderSizePixel = 0
-    Instances.TextLabel_2.Text = Client.Name
-    Instances.TextLabel_2.TextColor3 = Colors.Text
-    Instances.TextLabel_2.TextSize = 14
-    Instances.TextLabel_2.Font = Enum.Font.GothamSemibold
-    Instances.TextLabel_2.TextXAlignment = Enum.TextXAlignment.Right
-    Instances.TextLabel_2.Position = UDim2.new(0.38367345929145813, 0, 0, 0)
-    Instances.TextLabel_2.Size = UDim2.new(0, 257, 0, 30)
+    TextLabel_2.Name = "UserName"
+    TextLabel_2.Parent = Frame_1
+    TextLabel_2.BackgroundTransparency = 1
+    TextLabel_2.BorderSizePixel = 0
+    TextLabel_2.Text = Client.Name
+    TextLabel_2.TextColor3 = Colors.Text
+    TextLabel_2.TextSize = 14
+    TextLabel_2.Font = Enum.Font.GothamSemibold
+    TextLabel_2.TextXAlignment = Enum.TextXAlignment.Right
+    TextLabel_2.Position = UDim2.new(0.38367345929145813, 0, 0, 0)
+    TextLabel_2.Size = UDim2.new(0, 257, 0, 30)
 
-    Instances.Frame_3.Name = "Splitter_2"
-    Instances.Frame_3.Parent = Instances.Frame_1
-    Instances.Frame_3.BorderSizePixel = 0
-    Instances.Frame_3.Position = UDim2.new(0.37959182262420654, 0, 0, 0)
-    Instances.Frame_3.Size = UDim2.new(0, 2, 0, 30)
+    Frame_3.Name = "Splitter_2"
+    Frame_3.Parent = Frame_1
+    Frame_3.BorderSizePixel = 0
+    Frame_3.Position = UDim2.new(0.37959182262420654, 0, 0, 0)
+    Frame_3.Size = UDim2.new(0, 2, 0, 30)
 
-    Instances.UIGradient_2.Name = "UIGradient_2"
-    Instances.UIGradient_2.Rotation = 190
-    Instances.UIGradient_2.Color = ColorSequence.new{
+    UIGradient_2.Name = "UIGradient_2"
+    UIGradient_2.Rotation = 190
+    UIGradient_2.Color = ColorSequence.new{
         ColorSequenceKeypoint.new(0, Color3.fromRGB(78, 39, 39)),
         ColorSequenceKeypoint.new(0.72213, Color3.fromRGB(54, 51, 51)),
         ColorSequenceKeypoint.new(1, Color3.fromRGB(94, 94, 94))
